043a424a3a810acd675c2c64a9d18a0f529cfb04
daiye <411642610@qq.com>
2023-10-18 15:18:22 +0800

    Movies

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        //[DataType] 属性，用于指定 ReleaseDate 属性中的数据类型。 通过此特性：
+        //用户无需在日期字段中输入时间信息。
+        //仅显示日期，而非时间信息。
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -12,23 +12,23 @@ namespace RazorPagesMovie
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (!app.Environment.IsDevelopment())//当应用未在开发模式中运行时
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Error");//将异常终结点设置为 /Error
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts();//启用 HTTP 严格传输安全协议 (HSTS)
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseHttpsRedirection();//将 HTTP 请求重定向到 HTTPS。
+            app.UseStaticFiles();//使能够提供 HTML、CSS、映像和 JavaScript 等静态文件
 
-            app.UseRouting();
+            app.UseRouting();//向中间件管道添加路由匹配
 
-            app.UseAuthorization();
+            app.UseAuthorization();//授权用户访问安全资源。
 
-            app.MapRazorPages();
+            app.MapRazorPages();//为 Razor Pages 配置终结点路由。
 
-            app.Run();
+            app.Run();//运行应用。
         }
     }
 }
