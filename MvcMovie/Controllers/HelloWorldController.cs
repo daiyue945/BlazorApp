062a320ebdf898fef53fbd27d12f6480d8e09a08
daiye <411642610@qq.com>
2023-10-20 16:24:02 +0800

    2

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes =1)
+        {
+            //return "This is the Welcome action method...";
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = $"hello {name}";
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+        public string Welcome2(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            
+        }
+
+    }
+}
