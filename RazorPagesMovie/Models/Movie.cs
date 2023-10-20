b3ee1261a25adb8cfe8cee3578dcbcf3100683e4
daiye <411642610@qq.com>
2023-10-20 12:28:23 +0800

    razorMovie

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,15 +6,27 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; } = string.Empty;
         
-        [Display(Name = "Release Date")]//[Display] 属性指定字段的显示名称
+        [Display(Name = "Release Date"),DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]//[Display] 属性指定字段的显示名称
         [DataType(DataType.Date)]//[DataType] 属性，用于指定 ReleaseDate 属性中的数据类型。 通过此特性：用户无需在日期字段中输入时间信息。仅显示日期，而非时间信息。
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; } = string.Empty;
 
+        [Range(0,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName="decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; } = string.Empty;
     }
 }
