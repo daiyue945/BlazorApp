bb08fe170ec4ad47c7bcd3e6de8a8dbd138dc5af
daiye <411642610@qq.com>
2023-10-24 16:14:44 +0800

    基于控制器的WebAPI和面向javascript的WebAPI

---
+++ TodoApi/Program.cs
@@ -0,0 +1,45 @@
+
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            //指定数据库上下文将使用内存中数据库。
+            builder.Services.AddDbContext<TodoContext>(option => {
+                option.UseInMemoryDatabase("TodoList");
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
