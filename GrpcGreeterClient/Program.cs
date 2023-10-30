fd7c17e9c2812b7b2f009f2b95eab4e2e0d6d59e
daiye <411642610@qq.com>
2023-10-30 16:11:51 +0800

    GRPC

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using System.Threading.Tasks;
+using GrpcGreeterClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7264");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "GreeterClient" }
+    );
+Console.WriteLine("Greeting:" + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
