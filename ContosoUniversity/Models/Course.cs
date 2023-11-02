36e4cf7e93f7f5faed11789f6fc51acdbdfedcda
daiye <411642610@qq.com>
2023-11-02 15:55:41 +0800

    复杂EF

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
@@ -6,10 +8,18 @@ namespace ContosoUniversity.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Number")]
         public int CourseID { get; set; }
+        [StringLength(50,MinimumLength =3)]
         public string Title { get; set; }
+        [Range(0,5)]
         public int Credits { get; set; }
 
+        public int DepartmentID { get; set; }
+        public Department Department { get; set; }
+        //参与一门课程的学生数量不定
         public ICollection<Enrollment> Enrollments { get; set; }
+        //一门课程可能有多位授课讲师
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
