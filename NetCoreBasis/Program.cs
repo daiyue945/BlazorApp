4b5d717387088cc23544b44911536a9009c4e65a
daiye <411642610@qq.com>
2023-11-14 16:03:49 +0800

    基础

---
+++ NetCoreBasis/Program.cs
@@ -0,0 +1,37 @@
+namespace NetCoreBasis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddControllersWithViews();
+            //×¢²áIStartupFilter
+            builder.Services.AddTransient<IStartupFilter, RequestSetOptionsStartupFilter>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
---
+++ NetCoreBasis/RequestSetOptionsMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace NetCoreBasis
+{
+    public class RequestSetOptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RequestSetOptionsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        //从查询字符串参数中设置选项值
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var option = httpContext.Request.Query["option"];
+            if (!string.IsNullOrWhiteSpace(option))
+            {
+                httpContext.Items["option"] = WebUtility.HtmlEncode(option);
+            }
+            await _next(httpContext);
+        }
+    }
+    //在 RequestSetOptionsStartupFilter 类中配置 RequestSetOptionsMiddleware
+    public class RequestSetOptionsStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return builder => { 
+                builder.UseMiddleware<RequestSetOptionsMiddleware>(next);
+                next(builder);
+            };
+        }
+    }
+
+
+}
