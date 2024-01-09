d2f1222099ec3c6367bd5f1a45d9fb462e3d85f8
daiye <411642610@qq.com>
2024-01-09 13:04:54 +0800

    增加自动化控制微信发消息项目

---
+++ 自动化控制微信发消息/Program.cs
@@ -0,0 +1,11 @@
+﻿using NetAutoGUI;
+namespace 自动化控制微信发消息
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
